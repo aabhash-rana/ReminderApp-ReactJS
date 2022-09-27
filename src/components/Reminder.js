39afc3d6f9dcb7c7566909fb3ea646b00f1d1c45
Aabhash Rana <aabhashbikramrana@gmail.com>
2022-09-26 19:56:53 -0500

    Second Commit

--- src/components/Reminder.js
+++ src/components/Reminder.js
@@ -3,11 +3,7 @@ import React, {} from "react";
 function Reminder(props) {
     return (
         <span>
-          {props.showCompletedReminders ? (
-            <s>{props.reminder.title}</s>
-          ) : (
-            <span>{props.reminder.title}</span>
-          )}
+         
         </span>
       );
     }
